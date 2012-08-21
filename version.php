42f63f47d3bb93b53d928136591b8bdbedb4871d
Andre Scherl <post@schoerlock.de>
2012-08-21 14:58:08 +0200

    updated version info

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
 
-$module->version  = 2011040600;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2012082100;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 1;           // Period for cron to check this module (secs)
 $module->requires = 2010112400; // Moodle 2.0
 $module->component = 'mod_decisiontree';
-$module->release = '1.0 (Build: 2011040600)';
+$module->release = '1.1 (Build: 2012082100)';
